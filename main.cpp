1615599a303f680e3987492f679554c0981b2508
pedrohenriquebraga <gamesantos7@gmail.com>
2021-04-13 00:57:01 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+	Usado para incluir bibliotecas.
+	A biblioteca IOStream é usado para o uso de comando básicos do C++.
+	Ela deve ser incluída no começo dos arquivos.
+*/
+#include <iostream>
+
+/* 
+	Indica onde estão às bibliotecas que serão utilizadas.
+	Bem parecido com organizção de pastas.
+*/
+using namespace std;
+
+int main() {
+	cout << "Hello World!";	
+	return 0;
+}
