522a3bc4bcab79d74b67cedacee913fa36e08d9c
pedrohenriquebraga <gamesantos7@gmail.com>
2021-04-13 18:47:38 -0300

    Class 6, invert values

---
+++ classes/aula-006 - Inverter valores/aula-006.cpp
@@ -0,0 +1,16 @@
+// Invertendo valores
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int num = 10; 
+	
+	// Método tradicional: num *= -1;
+	// Método simplificado: num = -num; | Ou pode ser feito no momento em que o valor negativo será utilizado |.
+	
+	cout << num << "\n";
+	return 0;
+}
+
