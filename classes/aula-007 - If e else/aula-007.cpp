06ed1b1cae6ba775a8edd217ba442bc4eaff2375
pedrohenriquebraga <gamesantos7@gmail.com>
2021-04-13 19:19:07 -0300

    Class 7, IF and ELSE

---
+++ classes/aula-007 - If e else/aula-007.cpp
@@ -0,0 +1,35 @@
+// Usando IF e ELSE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int num1 = 10;
+	int num2 = 50;
+	
+	/*
+		OPERADORES:
+			> = maior
+			< = menor
+			>= = maior ou igual
+			<= = menor ou igual
+			== = igual
+			!= = diferente
+	*/
+	
+	if (num1 < num2) {
+		// Se o teste for verdadeiro ele executa este bloco de código
+		
+		cout << "Resultado VERDADEIRO";
+		
+	} else {
+		// Se o teste for falso ele executa este bloco de código
+		
+		cout << "Resultado FALSO";
+	};
+	
+	return 0;
+}
+
+
