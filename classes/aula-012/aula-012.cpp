4057fa28a970438304c3d58d02797db17467133e
pedrohenriquebraga <gamesantos7@gmail.com>
2021-04-14 10:40:53 -0300

    Class 11, while

--- classes/aula-012/aula-012.cpp
+++ classes/aula-012/aula-012.cpp
@@ -1,8 +1,21 @@
+// Usando estrutura while
+
 #include <iostream>
 using namespace std;
 
 int main() {
-	
+  int cont = 0;
+
+  // Laço de repetição usado para fazer tarefas em quantidades definidas ou não.
+
+  while (cont < 20) {
+    cout << "Testando - " << cont << "\n";
+    cont++;
+
+    // if (cont == 10) break;
+  };
+
+  cout << "<-- Rotina finalizada! -->";
 
 	return 0;
 }
