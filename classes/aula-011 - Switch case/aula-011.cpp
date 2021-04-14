75f2b6ce539606a0006e3bedc656cba65d4a667f
pedrohenriquebraga <gamesantos7@gmail.com>
2021-04-13 22:12:39 -0300

    Class 11, Switch Case

---
+++ classes/aula-011 - Switch case/aula-011.cpp
@@ -0,0 +1,49 @@
+// Usando Switch Case
+// Switch só testa casos de igualdade
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int val;
+	
+	cout << "Selecione um transporte: \n";
+	cout << "[1] Carro [2] Moto [3] Aviao [4] Helicoptero\n";
+	
+	cin >> val;
+	
+	switch(val) { 
+	  case 1:
+	  case 2:
+			cout << "\nTransporte terrestre\n";
+			switch(val) {
+				case 1:
+					cout << "Carro selecionado\n";
+					break;
+				case 2:
+					cout << "Moto selecionada\n";
+					break;
+			};
+			break;
+		case 3:
+		case 4:
+		  cout << "\nTransporte aereo\n";
+			switch(val) {
+				case 3:
+					cout << "Aviao selecionado\n";
+					break;
+				case 4:
+					cout << "Helicoptero selecionada\n";
+					break;
+			};
+			break;
+		default:
+			cout << "Cor selecionada: Nenhuma";
+			break;
+	}
+	
+	cout << "\n\n=== PROGRAMA FINALIZADO! ===";
+	
+	return 0;
+}
